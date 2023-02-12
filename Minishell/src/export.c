dc19cecd26a47e22437c5329d85576c5fddaecb9
stgerard <stgerard@student.42mulhouse.fr>
2023-02-12 15:07:19 +0100

    htgref

--- Minishell/src/export.c
+++ Minishell/src/export.c
@@ -6,13 +6,13 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:07:27 by stgerard          #+#    #+#             */
-/*   Updated: 2023/02/12 12:48:25 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/02/12 15:05:12 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int			detect(char *actual_cmd, t_minishell *shell)
+char			*recupvar(char *actual_cmd)
 {
 	size_t	i;
 	size_t	j;
@@ -20,23 +20,37 @@ int			detect(char *actual_cmd, t_minishell *shell)
 
 	i = 0;
 	j = 0;
-	while (actual_cmd[1] != '=')
+	while (actual_cmd[i] != '=' && actual_cmd[i] != '\0')
 		i++;
-	newvar = malloc(sizeof(char *) * i + 1);
+	newvar = malloc(sizeof(char) * (i + 1));
 	while (j < i)
-		newvar[j++] = actual_cmd[j++];
-	printf("%s\n", newvar);
-	i = 0;
-	j = 0;
-	while (shell->env[j] != NULL)
 	{
-		while ()
-		if (newvar == shell->env[j])
-
-		new var 
-		if ()
+		newvar[j] = actual_cmd[j];
 		j++;
 	}
+	newvar[j] = '\0';
+	return (newvar);
+}
+
+int			detect(char *actual_cmd, t_minishell *shell)
+{
+	int		i;
+	int		j;
+	char	*varcmd;
+	char	*varenv;
+
+	i = 0;
+	j = 0;
+	varcmd = recupvar(actual_cmd);
+	while (shell->env[j] != NULL)
+	{
+		varenv = recupvar(shell->env[j]);
+		if (ft_strcmp(varcmd, varenv) == 0)
+			return (j);
+		else
+			j++;
+	}
+	return (-1);
 }
 
 void		addvar(char *actual_cmd, t_minishell *shell)
@@ -62,15 +76,25 @@ void		addvar(char *actual_cmd, t_minishell *shell)
 
 int	ft_export(t_node *actual_cmd, t_minishell *shell)
 {
+	int	i;
+
+	i = 0;
 	if (actual_cmd->cmd && !actual_cmd->cmd[1])
 		ft_env(shell);
-	else if (actual_cmd && detect(actual_cmd->cmd[1], shell) == 1)
-		{
-			printf("detected !\n");
-			// unset puis set
-		}
 	else
-		addvar(actual_cmd->cmd[1], shell);
+	{
+		while (actual_cmd->cmd[++i] != NULL)
+		{
+			if (actual_cmd && detect(actual_cmd->cmd[i], shell) >= 0)
+			{
+				printf("detected !\n");
+				
+				// unset puis addvar
+			}
+			else
+				addvar(actual_cmd->cmd[1], shell);
+		}
+	}
 	return 0;
 } // pas encore, un tout petit debut de fct
 
