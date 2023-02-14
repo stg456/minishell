5a2bbfa64f7972b709fb5dfdc03cb814353f9ca1
stgerard <stgerard@student.42mulhouse.fr>
2023-02-14 18:07:30 +0100

    hio

--- Minishell/src/builtins.c
+++ Minishell/src/builtins.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 16:46:01 by stgerard          #+#    #+#             */
-/*   Updated: 2023/02/14 17:27:37 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/02/14 18:06:55 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,19 +80,15 @@ int	ft_cd(t_minishell *shell, t_node *lst)
 		}
 		else if (lst->cmd[j][0] == '~' && lst->cmd[j][1] == '/')
 		{
-			// lst->cmd[j] = lst->cmd[j + 1];
-			chdir(lst->cmd[j]);
-			printf("new cd: %s\n", lst->cmd[j + 1]);
+			i = chdir(lst->cmd[j]);
+			chdir(lst->token);
+			printf("new cd: %s\n", lst->cmd[++j]);
 			while (lst->cmd[j] && a != '\0')
 			{
 				lst->cmd[k][a] = lst->cmd[j][a + 2];
 				a++;
 			}
-			// lst->cmd[k][a] = 0;
-			// lst->cmd[j] = lst->cmd[k];
-			// printf("cd: %s\n", lst->cmd[k]);
-			// chdir(shell->dir);
-			// chdir(lst->cmd[k]);
+
 		}
 		else
 		{
@@ -111,7 +107,9 @@ int	ft_cd(t_minishell *shell, t_node *lst)
 // le cd .. ne marche plus , il fait 2 fois cd .. !!!
 
 // pour cd me doit pas pouvoir prendre d'argument supplementaire
+// pour echo si a=123 le $a = 1 2 3 !!!
 // pour echo manque le $?
 // pour export manque le =''
 // manque le heredoc
 // manque le cas des quotes non fermes pour echo
+// odre alpha pour env
--- Minishell/src/echo.c
+++ Minishell/src/echo.c
@@ -6,7 +6,7 @@
 /*   By: stgerard <stgerard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 10:52:18 by stgerard          #+#    #+#             */
-/*   Updated: 2023/02/14 13:22:23 by stgerard         ###   ########.fr       */
+/*   Updated: 2023/02/14 17:52:36 by stgerard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,8 +80,8 @@ void	affecho(t_node *lst, size_t	i, t_minishell *shell)
 		{
 			lst->cmd[i] = ft_strtok(lst->cmd[i], "\"", 7);
 			printf("%s", lst->cmd[i]);
-			if (lst->cmd[i + 1] && lst->cmd[i])
-				printf(" ");
+			// if (lst->cmd[i + 1] && lst->cmd[i])
+				// printf(" ");
 		}
 	}
 }
